30d6c485adb1975bc0fc1cdb717f59f2eece10e9
MerylCordier <merylcordier@gmail.com>
2024-10-09 12:58:07 +0200

    feat(rules for password): checkbox comportment ok

--- src/components/Common/Captcha/index .jsx
+++ src/components/Common/Captcha/index .jsx
@@ -1,31 +1,32 @@
-import React from 'react';
-import ReCAPTCHA from "react-google-recaptcha"; 
-import { useEffect, useRef } from 'react';
+import React from "react";
+import ReCAPTCHA from "react-google-recaptcha";
+import { useEffect, useRef } from "react";
 
 function Captcha({ onData }) {
   const [isCaptchaSuccessful, setIsCaptchaSuccess] = React.useState(false);
-  const recaptcha = useRef(); 
- 
-  function onChange() {   
+  const recaptcha = useRef();
+
+  function onChange() {
     const recaptchaValue = recaptcha.current.getValue();
-    if (!recaptchaValue){
-      toast.error('Veuillez valider le captcha');
-    }     
-    onData({isCaptchaSuccessful, recaptchaValue});
-  };
+    if (!recaptchaValue) {
+      toast.error("Veuillez valider le captcha");
+    }
+    onData({ isCaptchaSuccessful, recaptchaValue });
+  }
 
   useEffect(() => {
-    setIsCaptchaSuccess(true);}, [recaptcha]);
+    setIsCaptchaSuccess(true);
+  }, [recaptcha]);
 
   return (
     <ReCAPTCHA
-      className='recaptcha'
       ref={recaptcha}
       sitekey={import.meta.env.VITE_RECAPTCHA_SITE_KEY}
-      size='normal'
-      onChange={onChange}                              
-    />   
+      size="compact"
+      theme="dark"
+      onChange={onChange}
+    />
   );
-};
+}
 
 export default Captcha;
